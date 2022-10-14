178ed6422514972b901e4bf3ada06062c1cd48e5
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-10-15 00:27:16 +0500

    MongoDB Connection

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO_URL
+  )
+  .then(() => console.log("Db Connection Successfully."))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running.");
+});
